4f6d573f38ef6e4da80ffdc3dc514e7eb12d01ba
Pablo Bobo Student <201600206@alu.comillas.edu>
2021-06-09 13:48:12 +0200

    Cambios en el encabezado

--- script/Manejo_de_datos.py
+++ script/Manejo_de_datos.py
@@ -6,4 +6,17 @@
 """
 
 def lectura_nombre_columnas(url=None, num=-1):
-    
+    """
+    Función que crea un archivo txt con los headers del archivo csv indicado
+    url: String con la dirección de memoria del archivo a obtener headers
+    num: Número de documento en dónde almacenar los datos tal que: columnasnum.txt
+    """
+    import sys  # comprobación de librerías adecuadas
+    modulename = "pd"
+    if modulename not in sys.modules:
+        import pandas as pd
+
+    if url == None:  # Obtención de url de datos
+        route = input("Introduzca la url desde dónde desea extraer los datos: ")
+    else:
+        route = url
