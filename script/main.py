5045e1888a2d8a8d03318b124a701cf4ee064718
Pablo Bobo Student <201600206@alu.comillas.edu>
2021-06-09 11:34:24 +0200

    Primer inicio a documentación

--- script/main.py
+++ script/main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+'''
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    Autores: Blanca Gonzalez, Gonzalo García, Nicolás Olabarría y Pablo Bobo
+    Fecha: 9 de Junio de 2021
+    Tema: Manejo de datos Confidenciales
+    
+'''
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
