f6ffd2ede121f22f762ffd4902ac88d8ad15a90d
Pablo Bobo Student <201600206@alu.comillas.edu>
2021-06-10 13:36:53 +0200

    Comprobación lugar de almacenaje
    lectura_nombre_columnas() comprobará que la dirección de almacenaje del archivo con los títulos de las columnas está vacío. En caso de que no sea así, se preguntará al usuario si desea sobreescribir los datos en el documento.

--- script/Manejo_de_datos.py
+++ script/Manejo_de_datos.py
@@ -1,15 +1,17 @@
 """
-    Autores: Pablo Bobo
+    Autores: Nicolás Olabarría y Pablo Bobo
     Fecha: 9 de Junio de 2021
     Tema: Manejo de Datos
 
 """
 
-def lectura_nombre_columnas(url=None, num=-1):
+messageOption = '¿Quieres sobreescribir este archivo?[yes/no]: '
+messageNotValid = 'Opción no válida. Introdúzcalo de nuevo\n'
+
+def lectura_nombre_columnas(url, num=-1):
     """
     Función que crea un archivo txt con los headers del archivo csv indicado .
-    :param url: String con la dirección de memoria del archivo a obtener headers. En caso de no incluirse url, la función
-            pedirá al usuario introducir una nueva url .
+    :param url: String con la dirección de memoria del archivo a obtener headers.
            num: Número de documento en dónde almacenar los datos tal que: columnas'num'.txt, en caso de ser negativo el archivo
             tendrá nombre Columnas.txt .
     :return: Nullo
@@ -19,12 +21,7 @@ def lectura_nombre_columnas(url=None, num=-1):
     if modulename not in sys.modules:
         import pandas as pd
 
-    if url == None:  # Obtención de url de datos
-        route = input("Introduzca la url desde dónde desea extraer los datos: ")
-    else:
-        route = url
-
-    columnas = pd.read_csv(route, sep=";", header=None,
+    columnas = pd.read_csv(url, sep=";", header=None,
                            dayfirst=True, infer_datetime_format=True, nrows=1)
 
     # Escritura de archivos txt
@@ -33,10 +30,32 @@ def lectura_nombre_columnas(url=None, num=-1):
     else:
         number = str(num)
         title = "Columnas" + number + ".txt"
-    with open(title, "w+") as file1:
-        for i in range(len(columnas.columns)):
-            file1.write(columnas[i][0])
-            file1.write(',')
+
+    #Comprobación de ruta vacía
+    from os import path
+    print("Los archivos actuales se almacenarán en: ", path.dirname(path.abspath(__file__)))
+    route = path.dirname(path.abspath(__file__)) + r'\\' + title
+    if path.exists(route):
+        print("La ruta señalada está ya en uso.\n")
+        option = input(messageOption)
+        while option.upper() != 'YES' and option.upper() != 'NO':
+            print(messageNotValid)
+            option = input(messageOption)
+        if option.upper() == "YES":
+            from os import remove
+            remove(route)
+            with open(title, "w+") as file1:
+                for i in range(len(columnas.columns)):
+                    file1.write(columnas[i][0])
+                    file1.write(',')
+        else:
+            print("Las columnas del archivo no serán almacenadas en el archivo con nombre: ", title)
+    else:
+        with open(title, "w+") as file1:
+            for i in range(len(columnas.columns)):
+                file1.write(columnas[i][0])
+                file1.write(',')
+
 
 def headers_varios_archivos(lista):
     """
